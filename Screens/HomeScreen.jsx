b53f601b93139e5fcf85b1f776bfcbfcd51d0119
karmjeetbanga <karmjeet.k@adorebeauty.com.au>
2022-10-16 23:53:39 +1100

    Added suggestion list feature in search bar

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,58 +1,105 @@
-import React, {useEffect, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TextInput,Button,Keyboard, TouchableOpacity} from 'react-native';
-import axios from 'axios';
- 
-export default function HomeScreen(){
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, Dimensions } from "react-native";
+import axios from "axios";
+import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
+import Feather from "react-native-vector-icons/Feather";
+Feather.loadFont();
 
-    const [historicalEventsData, setHistoricalEventsData] = useState([]);
-    const [filteredData,setFilteredData] = useState(historicalEventsData);
- 
-     const handleSearch = (searchKeywords) => {
-      let value = searchKeywords.toLowerCase();
+export default function HomeScreen() {
+  const [historicalEventsData, setHistoricalEventsData] = useState([]);
+  const [filteredData, setFilteredData] = useState(historicalEventsData);
+  const [searchSuggestionData, setSearchSuggestionData] = useState([]);
 
-      let result = historicalEventsData.filter((data) => {
-        if(data.category2) {
-           return data.category2.toLowerCase().search(value) != -1;
-        }
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/karmjeetbanga/event-history/main/eventHistoryData.json"
+      )
+      .then((response) => {
+        setHistoricalEventsData(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
       });
+  }, []);
 
-      setFilteredData(result);
-    };
+  const handleSearch = (searchKeywords) => {
+    let value = searchKeywords.toLowerCase();
+    let result = historicalEventsData.filter((data) => {
+      if (data.category2) {
+        return data.category2.toLowerCase().search(value) != -1;
+      }
+    });
+    setFilteredData(result);
+  };
 
-      useEffect(() => {
-        axios.get('https://raw.githubusercontent.com/karmjeetbanga/event-history/main/eventHistoryData.json')
-        .then((response) => {
-         // console.log(response.data);
-            setHistoricalEventsData(response.data);
-            setFilteredData(response.data);
-        })
-        .catch((error) => {
-            console.log('Error: ' + error);
-        })
-    }, [])
+  const onSearchBarClearEvent = () => {
+    setFilteredData(historicalEventsData);
+  };
 
-const renderItem = ({ item }) => (
+  const getSuggestionList = () => {
+    let category2 = historicalEventsData
+      .map((item) => {
+        return item.category2;
+      })
+      .filter((item) => {
+        return item != null || item != undefined;
+      })
+      .sort();
+
+    let uniqueCategory2 = [...new Set(category2)];
+
+    let searchKeywords = uniqueCategory2.map((item, index) => {
+      return { id: index, title: item };
+    });
+
+    setSearchSuggestionData(searchKeywords);
+  };
+
+  const onOpenSuggestionsList = () => {
+    if (searchSuggestionData.length === 0) {
+      getSuggestionList();
+    }
+  };
+
+  const renderItem = ({ item }) => (
     <View style={styles.listContainer}>
       <Text style={styles.headeStyle}> {item.category2}</Text>
       <Text style={styles.textStyle}> {item.description}</Text>
     </View>
   );
 
- return (
+  return (
     <View style={styles.container}>
-    <TextInput 
-          style={styles.input} 
-          placeholder="Search history by place (eg. Asia, Africa etc.)"
-          value={filteredData}
-          onChangeText={(event) =>handleSearch(event)}
-          />
-     <FlatList
+      <AutocompleteDropdown
+        inputContainerStyle={styles.searchBar}
+        suggestionsListContainerStyle={styles.suggestionsListContainer}
+        onChangeText={(event) => handleSearch(event)}
+        onSelectItem={(item) => {
+          item && handleSearch(item.title);
+        }}
+        onClear={onSearchBarClearEvent}
+        dataSet={searchSuggestionData}
+        clearOnFocus={false}
+        closeOnBlur={true}
+        closeOnSubmit={true}
+        debounce={300}
+        textInputProps={{
+          placeholder: "Search history by place (eg. Asia, Africa etc.)",
+          autoCorrect: true,
+          autoCapitalize: "none",
+        }}
+        onOpenSuggestionsList={onOpenSuggestionsList}
+      />
+      <FlatList
         data={filteredData}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         ItemSeparatorComponent={() => <View style={styles.Separator} />}
       />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -61,19 +108,20 @@ const renderItem = ({ item }) => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }, 
+    backgroundColor: "white",
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+    paddingTop: 10,
+  },
   Separator: {
-        borderBottomColor:'rgb(230, 236, 240)',
-        borderBottomWidth: 1,
+    borderBottomColor: "rgb(230, 236, 240)",
+    borderBottomWidth: 1,
   },
   listContainer: {
     flex: 1,
-    backgroundColor: '#F8F9F9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F8F9F9",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
@@ -86,17 +134,17 @@ const styles = StyleSheet.create({
   },
   headeStyle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: 'rgb(0, 0, 0)',
+    fontWeight: "bold",
+    color: "rgb(0, 0, 0)",
     padding: 10,
   },
-   textStyle: {
-    flexWrap: 'wrap',
+  textStyle: {
+    flexWrap: "wrap",
     paddingBottom: 5,
     paddingTop: 5,
     fontSize: 16,
-    color: 'rgb(0, 0, 0)',  
-},
+    color: "rgb(0, 0, 0)",
+  },
   searchContainer: {
     flexDirection: "row",
     alignItems: "center",
@@ -106,18 +154,36 @@ const styles = StyleSheet.create({
     margin: 10,
     backgroundColor: "rgb(230, 236, 240)",
     padding: 10,
-},
-  input: {
-    width: "90%",
-    margin: 12,
-    borderWidth: 1,
+  },
+  itemText: {
+    fontSize: 16,
+    color: "rgb(0, 0, 0)",
+    padding: 10,
+  },
+  item: {
+    backgroundColor: "#F8F9F9",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
     borderRadius: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "rgb(0, 0, 0)",
     padding: 10,
+  },
+  searchBar: {
+    margin: 20,
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 6,
     backgroundColor: "rgb(230, 236, 240)",
-    alignItems: "center",
   },
-  searchIcon: {
-    padding: 10,
+  suggestionsListContainer: {
+    width: "90%",
+    marginLeft: 20,
+    marginTop: 30,
+    backgroundColor: "rgb(230, 236, 240)",
   },
-
 });
